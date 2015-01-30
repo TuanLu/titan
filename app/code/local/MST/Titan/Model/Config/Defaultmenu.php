4221aec4a88f9c18fff7d733eec221b0151a1998
Calvin Cao <vigentocommerce@gmail.com>
2015-01-30 11:33:27 +0700

    Change system.xml
    - Remove customization field
    - Change some text lable

--- app/code/local/MST/Titan/Model/Config/Defaultmenu.php
+++ app/code/local/MST/Titan/Model/Config/Defaultmenu.php
@@ -3,9 +3,9 @@ class MST_Titan_Model_Config_Defaultmenu {
 	public function toOptionArray()
     {
 		$navs = array();
-		$navs[] = array('value'=> 'default', 'label'=>Mage::helper('titan')->__('Magento Default Menu'));
+		$navs[] = array('value'=> 'default', 'label'=>Mage::helper('titan')->__('Default Template Menu'));
 		if (Mage::helper('core')->isModuleEnabled('MST_Menupro')) {
-			$navs[] = array('value'=> 'mcp', 'label'=>Mage::helper('titan')->__('MCP - Menu Creator Pro'));
+			$navs[] = array('value'=> 'mcp', 'label'=>Mage::helper('titan')->__('Use Menu Creator Pro'));
 		}
 		return $navs;
     }
--- app/code/local/MST/Titan/Model/Config/Logotype.php
+++ app/code/local/MST/Titan/Model/Config/Logotype.php
@@ -3,9 +3,9 @@ class MST_Titan_Model_Config_Logotype {
 	public function toOptionArray()
     {
         return array(
-            array('value'=> 'default', 'label'=>Mage::helper('titan')->__('Default Magento Setting')),
-			array('value'=> 'image', 'label'=>Mage::helper('titan')->__('Image')),
-			array('value'=> 'text', 'label'=>Mage::helper('titan')->__('Text')),
+            array('value'=> 'default', 'label'=>Mage::helper('titan')->__('Default (General > Design > Header)')),
+			array('value'=> 'image', 'label'=>Mage::helper('titan')->__('Logo Image')),
+			array('value'=> 'text', 'label'=>Mage::helper('titan')->__('Use Text')),
         );
     }
 }
