323635420412a7c834facb2158529de791fff8e5
Calvin Cao <vigentocommerce@gmail.com>
2015-01-30 16:44:53 +0700

    Remove not used files and modules

--- app/code/local/MST/Titan/Model/Config/Styles.php
+++ app/code/local/MST/Titan/Model/Config/Styles.php
@@ -8,12 +8,6 @@ class MST_Titan_Model_Config_Styles {
 			array('value'=> 3, 'label'=>Mage::helper('titan')->__('Skin 3')),
 			array('value'=> 4, 'label'=>Mage::helper('titan')->__('Skin 4')),
 			array('value'=> 5, 'label'=>Mage::helper('titan')->__('Skin 5')),
-			array('value'=> 6, 'label'=>Mage::helper('titan')->__('Skin 6')),
-			array('value'=> 7, 'label'=>Mage::helper('titan')->__('Skin 7')),
-			array('value'=> 8, 'label'=>Mage::helper('titan')->__('Skin 8')),
-			array('value'=> 9, 'label'=>Mage::helper('titan')->__('Skin 9')),
-			array('value'=> 10, 'label'=>Mage::helper('titan')->__('Skin 10')),
-			array('value'=> 11, 'label'=>Mage::helper('titan')->__('Skin 11')),
             
         );
     }
--- skin/frontend/t2/default/titan/js/main.js
+++
@@ -1,8 +0,0 @@
-/*! Main.js
-* Copyright 2014 Magebay.com
-*/
-var mb = jQuery.noConflict();
-mb(document).ready(function(){
-	 
-});
-
